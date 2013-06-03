48132e09386b24d74a0b376e253c2b7e9be7a2f2
BWZ <ben@delonge.com.au>
2013-06-03 18:09:18 +1000

    added groupids to password class

--- core/Password.php
+++ core/Password.php
@@ -11,6 +11,8 @@ class Password {
 	private $id;
 	// the timestamp of the password being entered or changed
 	private $timestamp;
+	// group password belongs to (0 for individual)
+	private $groupid;
 
 	// encrypted password data
 	private $title_enc;
@@ -27,18 +29,19 @@ class Password {
 	}
 
 	// save encrypted password details in the database
-	public function storePassword($title, $username, $pasword, $notes, $url) {
+	public function storePassword($title, $username, $pasword, $notes, $url, $groupid = 0) {
 		$this->title_enc = $title;
 		$this->username_enc = $username;
 		$this->password_enc = $pasword;
 		$this->notes_enc = $notes;
 		$this->url_enc = $url;
+		$this->groupid = $groupid;
 
 		$t = time();
 
-		$query = $this->db->prepare("INSERT INTO password (user_id, title_enc, username_enc, password_enc, notes_enc, url_enc, timestamp) VALUES(?, ?, ?, ?, ?, ?, ?)");
+		$query = $this->db->prepare("INSERT INTO password (user_id, title_enc, username_enc, password_enc, notes_enc, url_enc, timestamp, groupid) VALUES(?, ?, ?, ?, ?, ?, ?, ?)");
 		$uid = $this->user->userid;
-		$query->bind_param('sssssss', $uid, $this->title_enc, $this->username_enc, $this->password_enc, $this->notes_enc, $this->url_enc, $t);
+		$query->bind_param('ssssssss', $uid, $this->title_enc, $this->username_enc, $this->password_enc, $this->notes_enc, $this->url_enc, $t, $this->groupid);
 		$query->execute();
 		$this->id = $query->insert_id;
 
@@ -47,12 +50,12 @@ class Password {
 
 	// get a list of encrypted password details for the user provided
 	public function getPasswordList() {
-		$query = $this->db->prepare("SELECT passwordid, title_enc, username_enc, password_enc, notes_enc, url_enc, timestamp FROM password WHERE user_id = ?");
+		$query = $this->db->prepare("SELECT passwordid, title_enc, username_enc, password_enc, notes_enc, url_enc, timestamp, group_id FROM password WHERE user_id = ?");
 		$uid = $this->user->userid;
 		$query->bind_param('s', $uid);
 		$query->execute();
 
-		$query->bind_result($a, $b, $c, $d, $e, $f, $g);
+		$query->bind_result($a, $b, $c, $d, $e, $f, $g, $h);
 		$password_list = array();
 
 		while ($query->fetch()) {
@@ -63,6 +66,7 @@ class Password {
 			$this->password_enc = $d;
 			$this->notes_enc = $e;
 			$this->url_enc = $f;
+			$this->groupid = $h;
 
 			$p = array("id"=>$this->id, "title"=>$this->title_enc, "username"=>$this->username_enc, "password"=>$this->password_enc,
 				"notes"=>$this->notes_enc, "url"=>$this->url_enc, "timestamp"=>$this->timestamp);
